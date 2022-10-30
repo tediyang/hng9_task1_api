bad88c70c82aad3d5ba25ae8ae2df09ec9a0f02d
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-10-30 22:31:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home_page():
-    my_data = {'slackUsername': 'Eyang, Daniel Eyoh', \
-        'backend': True, 'age': 26, \
-        'bio': "I'm Eyang, Daniel Eyoh, an Entry Backend \
-        Software and Machine Learning Engineer (Python)  \
-        who loves building models and computer software. \
-        I'm working on an app using CNN that let users get \
-        the names of cars' Images and legal locations where they can purchase them. \
-        Despite having a B.Sc. degree in Geology, I combined my passion for learning \
-        and development, channelled it to Data Science & Software Development and acquired\
-        skills which helps me in building personalized software for people."}
+    my_data = {'slackUsername': 'Eyang, Daniel Eyoh', 'backend': True, 'age': 26, 'bio': "I'm Eyang, Daniel Eyoh, an Entry Backend Software and Machine Learning Engineer (Python) who loves building models and computer software. I'm working on an app using CNN that let users get the names of cars' Images and legal locations where they can purchase them. Despite having a B.Sc. degree in Geology, I combined my passion for learning and development, channelled it to Data Science & Software Development and acquired skills which helps me in building personalized software for people."}
 
     return my_data
 
