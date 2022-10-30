6a3386d4c2fee0e61b54a507e8d959c88991c987
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-10-30 19:38:04 +0100

    Update and rename app.py.py to app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
-from flask import *
+import os
+from flask import Flask
 import json
 
-app =Flask(__name__)
+app = Flask(__name__)
+DEPLOY = os.getenv('DEPLOY')
 
 @app.route('/', methods=['GET'])
 def home_page():
