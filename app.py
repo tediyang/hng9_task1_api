7fe23713d4048051153ebe7d32f70e85c65004a9
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-11-05 00:29:27 +0100

    POST without GET

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def home_page():
-    '''Get request taking you directly to homepage'''
-    my_data = {'slackUsername': 'Eyang, Daniel Eyoh',
-                   'backend': True, 'age': 26,
-                   'bio': "I'm Eyang, Daniel Eyoh, an Entry Backend Software and \
-                    Machine Learning Engineer (Python) who loves building models and \
-                    computer software. I'm working on an app using CNN that let users \
-                    get the names of cars' Images and legal locations where they can purchase them. \
-                    Despite having a B.Sc. degree in Geology, I combined my passion for learning \
-                    and development, channelled it to Data Science & Software Development and \
-                    acquired skills which helps me in building personalized software for people."}
-    return my_data
+# @app.route('/', methods=['GET'])
+# def home_page():
+#     '''Get request taking you directly to homepage'''
+#     my_data = {'slackUsername': 'Eyang, Daniel Eyoh',
+#                    'backend': True, 'age': 26,
+#                    'bio': "I'm Eyang, Daniel Eyoh, an Entry Backend Software and \
+#                     Machine Learning Engineer (Python) who loves building models and \
+#                     computer software. I'm working on an app using CNN that let users \
+#                     get the names of cars' Images and legal locations where they can purchase them. \
+#                     Despite having a B.Sc. degree in Geology, I combined my passion for learning \
+#                     and development, channelled it to Data Science & Software Development and \
+#                     acquired skills which helps me in building personalized software for people."}
+#     return my_data
 
 @app.route('/', methods=['POST'])
 def home_page_post():
