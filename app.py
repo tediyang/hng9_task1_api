2c88caf96aef928097bb0ce3c23f11b04614e6d2
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-11-05 00:57:41 +0100

    POST without GET

--- app.py
+++ app.py
@@ -34,13 +34,7 @@ def home_page_post():
     if request.method == 'GET':
         my_data = {'slackUsername': 'Eyang, Daniel Eyoh',
                    'backend': True, 'age': 26,
-                   'bio': "I'm Eyang, Daniel Eyoh, an Entry Backend Software and \
-                    Machine Learning Engineer (Python) who loves building models and \
-                    computer software. I'm working on an app using CNN that let users \
-                    get the names of cars' Images and legal locations where they can purchase them. \
-                    Despite having a B.Sc. degree in Geology, I combined my passion for learning \
-                    and development, channelled it to Data Science & Software Development and \
-                    acquired skills which helps me in building personalized software for people."}
+                   'bio': "I'm Eyang, Daniel Eyoh, an Entry Backend Software and Machine Learning Engineer (Python) who loves building models and computer software. I'm working on an app using CNN that let users get the names of cars' Images and legal locations where they can purchase them. Despite having a B.Sc. degree in Geology, I combined my passion for learning and development, channelled it to Data Science & Software Development and acquired skills which helps me in building personalized software for people."}
         return my_data
 
 
